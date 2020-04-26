8f076949e5d0a6051844c49de2e379356a2fd769
Koray Ozkal <koryozkal@gmail.com>
2020-04-26 21:14:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,11 +22,13 @@ def self.new_by_name(song_name)
   song
 end 
 
-def seld.create_by_name(song_name)
-  song = self.new
-  song.name = song_name
-  song.all << song 
-end 
+def self.create_by_name(song_name)
+
+    song = self.new
+    song.name = song_name
+    self.all << song
+    song
+  end 
   
   
 
