66998d264223982e25eaf54a26c8028d58e3c611
Koray Ozkal <koryozkal@gmail.com>
2020-04-26 21:04:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,20 +9,12 @@ class Song
   def save
     self.class.all << self
   end
- 
-	def find_by_name(name)
-			self.all.find{|item|item.name == name}
-		end
+#####################################################
 
-		def find_or_create_by_name(name)
-			song = find_by_name(name)
-			if song == nil
-				self.create(name)
-			else
-				song
-			end
-		
+def song_create(song)
+  
 
-	end
 
+
+#####################################################
 end
